f070ff2f5c5fac7bf18fc5afd1bb439aebc50a39
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-05-30 22:07:40 +0800

    Update deploy.sh: add -p param & Change image url to local

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@
 . ./scripts/common.sh
 
 CLEAN=false
-REMOTE=false
+PULL=false
 
 for param in $*; do
     if [ ${param} = "-c" ]; then
         CLEAN=true
-    elif [ ${param} = "-r" ]; then
-        REMOTE=true
+    elif [ ${param} = "-p" ]; then
+        PULL=true
     elif [ ${param} = "-h" ]; then
         printf "Usage: $0 [OPTIONS]\n\n\
 OPTIONS:\n\
   -c  clean all stacks, persistent data and exit\n\
-  -r  deploy on remote server\n\
+  -p  pull images from registry before deployment\n\
   -h  show this help prompt\n"
         exit 0
     fi
@@ -26,11 +26,22 @@ if [ ${CLEAN} = true ]; then
     exit 0
 fi
 
-if [ ${REMOTE} != true ]; then
-    printf "Deploying...\n"
-    docker swarm init
-    docker stack deploy -c ${FILE_COMPOSE} ${STACK}
-    printf "Done.\n"
-else
-    printf "remote.\n"
+printf "Deploying...\n"
+if [ ${PULL} = true ]; then
+    printf "\n"
+    docker pull ${REGISTRY}/${NS}/${IMAGE_SERVICE}
+    docker tag ${REGISTRY}/${NS}/${IMAGE_SERVICE} ${IMAGE_SERVICE}
+    printf "\n"
+    docker pull ${REGISTRY}/${NS}/${IMAGE_PROXY}
+    docker tag ${REGISTRY}/${NS}/${IMAGE_PROXY} ${IMAGE_PROXY}
+    printf "\n"
+    docker pull ${REGISTRY}/${NS}/${IMAGE_CACHE}
+    docker tag ${REGISTRY}/${NS}/${IMAGE_CACHE} ${IMAGE_CACHE}
+    printf "\n"
+    docker pull ${REGISTRY}/${NS}/${IMAGE_DB}
+    docker tag ${REGISTRY}/${NS}/${IMAGE_DB} ${IMAGE_DB}
+    printf "\n"
 fi
+docker swarm init
+docker stack deploy -c ${FILE_COMPOSE} ${STACK}
+printf "Done.\n"
